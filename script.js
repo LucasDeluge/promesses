9e4c6aaceff23c61322548ce005a2bb1bd1ec9cc
LucasDeluge <deluge999@gmail.com>
2022-05-10 11:12:27 +0200

    ajouts bootstrap

--- script.js
+++ script.js
@@ -11,26 +11,27 @@ const actionForm = async (name) => {
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
-    let lstLng = [];
-    for (lang in country.languages) {
-        lstLng.push(country.languages[lang]);
-    }
 
     data.forEach((country) => {
         const img = document.createElement('img');
         const p = document.createElement('p');
+        p.className = 'nameCountry';
+        const p2 = document.createElement('p');
+        let lstLng = [];
+        for (lang in country.languages) {
+            lstLng.push(country.languages[lang]);
+        }
         if (country.capital) {
-            img.src = `${country.flags.png}`;
-            p.innerHTML = `${country.name.common} - ${country.capital[0]}  <br/>  habitant: ${country.population} <br/> Nom en francais: ${country.translations.fra.official}
-  <br/> Superficie: ${country.area} km²`;
+            p.innerHTML = `${country.name.common}`;
+            p2.innerHTML = `<img src="${country.flags.png}"></img> <br/> Capitale : ${country.capital[0]}  <br/>  Habitants : ${country.population} <br/> Nom en francais : ${country.translations.fra.official}
+            <br/> Superficie : ${country.area} km² <br/> Langues : ${lstLng.join(", ")}`;
         }
         else {
             p.textContent = `${country.name.common} - Pas de capitale`
         }
         result.appendChild(p);
+        result.appendChild(p2);
         result.appendChild(img)
         console.log(country);
     });
 }
-
-actionForm()
